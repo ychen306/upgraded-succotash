b3e85f7c66cda737e88916dc98d39b692ccfc526
Yishen Chen <tom@31-38-75.wireless.csail.mit.edu>
2019-07-23 21:15:42 -0400

    init

---
+++ sema-gen.py
@@ -0,0 +1,63 @@
+from parser import parser
+import xml.etree.ElementTree as ET
+import sys
+
+data_f = sys.argv[1]
+data_root = ET.parse(data_f)
+
+num_parsed = 0
+num_skipped = 0
+supported_insts = set()
+skipped_insts = set()
+for intrin in data_root.iter('intrinsic'):
+  cpuid = intrin.find('CPUID')
+  sema = intrin.find('operation') 
+  inst = intrin.find('instruction')
+  inst_form = None
+  if inst is not None:
+    inst_form = inst.attrib['name'], inst.attrib.get('form')
+  if not (intrin.attrib['name'].startswith('_mm') or
+      intrin.attrib['name'].startswith('_mm')):
+    continue
+  if 'str' in intrin.attrib['name']:
+    if inst is not None:
+      skipped_insts.add(inst_form)
+    num_skipped += 1
+    continue
+  #if cpuid is not None and cpuid.text in ('MPX', 'KNCNI'):
+  #  continue
+
+  if 'fixup' in intrin.attrib['name']:
+    if inst is not None:
+      skipped_insts.add(inst_form)
+    num_skipped += 1
+    continue
+  if 'round' in intrin.attrib['name']:
+    if inst is not None:
+      skipped_insts.add(inst_form)
+    num_skipped += 1
+    continue
+  if 'prefetch' in intrin.attrib['name']:
+    if inst is not None:
+      skipped_insts.add(inst_form)
+    num_skipped += 1
+    continue
+
+  if inst is not None and sema is not None:
+    try:
+      #if 'ELSE IF' in sema.text:
+      #  continue
+      parser.parse(sema.text)
+      supported_insts.add(inst_form)
+      num_parsed += 1
+    except SyntaxError:
+      print('Parsed', num_parsed, ' semantics, failling:')
+      print(sema.text)
+      print(intrin.attrib['name'])
+      break
+    print(intrin.attrib['name'])
+print('Parsed:', num_parsed,
+    'Skipped:', num_skipped,
+    'Num unique inst forms parsed:', len(supported_insts),
+    'Num inst forms skipped:', len(skipped_insts)
+    )
