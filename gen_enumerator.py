8afb72680c0f0ed7a79c34caf11c4d3a068b39d0
Yishen Chen <ychen306@mit.edu>
2019-11-19 21:13:41 -0500

    fix init

--- gen_enumerator.py
+++ gen_enumerator.py
@@ -197,9 +197,14 @@ def emit_inst_evaluations(target_size, sketch_graph, sketch_nodes, out, max_test
       out.write('static int active_%d_%d = 0;\n' % (inst_config.node_id, inst_config.group_id))
       if use_stoke:
         out.write('static int needs_update_%d_%d = 1;\n' % (inst_config.node_id, inst_config.group_id))
-      out.write('static int %s = 0;\n' % inst_config.name)
+      out.write('static int {name} = {init};\n'.format(
+        name=inst_config.name,
+        init='0' if use_stoke else '-1'
+        )
       for arg in inst_config.args:
-        out.write('static int %s = 0;\n' % arg.name)
+        out.write('static int {name} = {init};\n'.format(
+          name=arg.name,
+          init='0' if use_stoke else '-1')
   out.write('static unsigned long long num_evaluated = 0;\n')
 
   return inst_evaluations, liveins, configs
